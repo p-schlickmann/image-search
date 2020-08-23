7fa004e3ec53a89cc5d56d271b79acf02e7b9b85
Pedro <windowsxpedro@gmail.com>
2020-08-23 20:45:16 -0300

    fixed image display

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 
-import App from './components/App.js'
-
-
-ReactDOM.render(<App/>, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector('#root'));
